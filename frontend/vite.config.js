7d61aec431a523950e89af33cf4bdba5a3da51ea
Widoyo <widoyo@gmail.com>
2026-01-29 02:50:46 +0700

    feat: Update environment configuration and Docker setup for backend and frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/balai/bbwssumatera8/",
-  server: {
-    host: '0.0.0.0', // Agar bisa diakses dari localhost Mac
-    port: 5173,
-    watch: {
-      usePolling: true, // Sangat penting untuk Docker agar Hot Reload jalan
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: "/balai/bbwssumatera8/",
+    server: {
+      proxy: {
+        "/api/": {
+          target: env.VITE_API_TARGET,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+      host: '0.0.0.0', // Agar bisa diakses dari localhost Mac
+      port: 5173,
+      watch: {
+        usePolling: true, // Sangat penting untuk Docker agar Hot Reload jalan
+      },
     },
-  },
+  };
 });
+
